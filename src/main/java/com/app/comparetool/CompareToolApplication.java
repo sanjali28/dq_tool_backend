bf100a3349dc46a3db7182940d4e5b097ef10a4d
Sanjali <sanjalidhamale@gmail.com>
2025-06-30 21:38:11 +0530

    initial commit

---
+++ src/main/java/com/app/comparetool/CompareToolApplication.java
@@ -0,0 +1,13 @@
+package com.app.comparetool;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CompareToolApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CompareToolApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/app/comparetool/CompareToolApplicationTests.java
@@ -0,0 +1,13 @@
+package com.app.comparetool;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CompareToolApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
